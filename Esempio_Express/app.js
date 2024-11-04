2c4b736356a8587a18e5dd9654b101bd49462ead
jusvald vald <valderama.justin@iisgalvanimi.edu.it>
2024-11-04 13:44:29 +0000

    primoExpress

---
+++ Esempio_Express/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const path = require('path');
+var app = express();
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname, 'index.html')); //__dirname : Ritorna la cartel
+});
+
+app.get('/about',function(req,res){
+    res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartell
+    });
+
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+});
+  
+app.get('/',function(req,res){
+    res.sendFile('index.html'); });
